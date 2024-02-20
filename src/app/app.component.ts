3e14baabf0c5b992fb1c6cc43b82e66a219e6c5a
Neil-Barot <neil.r.barot@gmail.com>
2024-02-20 01:12:21 -0600

    ngStyle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PostComponent } from './post/post.component';
 import { FormsModule } from '@angular/forms';
-import { NgFor, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault} from '@angular/common';
+import { NgFor, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault, NgStyle} from '@angular/common';
 import { JsonPipe } from '@angular/common';
 
 
@@ -11,20 +11,20 @@ import { JsonPipe } from '@angular/common';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, NavbarComponent, PostComponent, FormsModule, 
-    NgFor, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault],
+    NgFor, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault, NgStyle],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = "world";
-  
-  stepForm: string = "Something Else";
 
+  isActive: boolean = true;
+  
   constructor(){
     
   }
 
-  onClick(status) {
-    this.stepForm = status;
+  onClick() {
+    this.isActive = !this.isActive;
   }
 }
