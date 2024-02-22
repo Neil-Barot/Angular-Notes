b4296ae96b0dce7f9b3bf07aff4b8d17b034a210
Neil-Barot <neil.r.barot@gmail.com>
2024-02-21 23:30:30 -0600

    CustomPipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,42 +3,30 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PostComponent } from './post/post.component';
 import { FormsModule } from '@angular/forms';
+import { AppendPipe } from './pipes/append.pipe';
 import { NgFor, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault, NgStyle, NgClass} from '@angular/common';
-import { JsonPipe } from '@angular/common';
+import { JsonPipe, UpperCasePipe, LowerCasePipe } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, PostComponent, FormsModule, 
-    NgFor, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault, NgStyle, NgClass],
+  imports: [RouterOutlet, NavbarComponent, PostComponent, AppendPipe,
+    FormsModule, UpperCasePipe, LowerCasePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = "world";
+  title: string = "Angular Course";
 
-  userName: string;
-  userEmail: string;
-  userAddress: string;
-
-  userArr: Array<any>;
+  userDetails = {
+    name: "User 1",
+    city: "New York",
+    country: "US"
+  }
 
   constructor(){
     
   }
 
-  onClick() {
-    console.log(this.userName);
-
-    this.userArr.push({
-      "name": this.userName, 
-      "email": this.userEmail, 
-      "address": this.userAddress
-    });
-  }
-
-  deleteObj(index) {
-    this.userArr.splice(index,1);
-  }
 }
